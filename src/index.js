dd3b96e0f0a22782c503c08161d6b3d4169d9edd
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-05-11 16:39:57 -0600

    :construction_worker: Pruebas con typescript

---
+++ src/index.js
@@ -0,0 +1,15 @@
+function sumar(numeroUno, numeroDos) {
+    return numeroUno + numeroDos
+}
+
+console.log(sumar(1, 5))
+
+console.log(sumar('1', 5))
+
+console.log(sumar('1', 'hola'))
+
+console.log(sumar(['1', '2'], true))
+
+function solicitarViaje(ubicaciones) {
+    // Al momento de crear un viaje, el viajeId se establece como Date.now(), para facilitar la distinción de cada viaje
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hola desde Typescript')
+function sumar(numeroUno: number, numeroDos: number) {
+    return numeroUno + numeroDos
+}
+
+function solicitarViaje(ubicaciones: number[]) {
+    // Al momento de crear un viaje, el viajeId se establece como Date.now(), para facilitar la distinción de cada viaje
+}
+
+solicitarViaje([1, 2, 3])
+
+let calificacionEstudiante: string = 'Aprobado'
+
+calificacionEstudiante = 'Satisfactorio'
+
+calificacionEstudiante = 'Bien hecho'
+
+let calificacionEstudianteMejorado: 'Aprobado' | 'No Aprobado' = 'Aprobado'
+
+calificacionEstudianteMejorado = 'No Aprobado'
